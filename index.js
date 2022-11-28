eaab61c92363c270ffe17c73453108c1ae8503fb
piotr-sadowski <77448516+piotr-sadowski@users.noreply.github.com>
2022-11-28 11:00:09 +0100

    Added comments to index.js

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
-// Requiring module
+// Requiring module. express is the framework
 const express = require("express");
 var path = require('path');
 
 const app = express();
 
 function authentication(req, res, next) {
+	// initialy req.headers.authorization is undefined, so next() call back function return 401 error is returned
+	// when user fill in user and pass (encrypted in base64)
 	var authheader = req.headers.authorization;
 	console.log(req.headers);
 
@@ -15,12 +17,14 @@ function authentication(req, res, next) {
 		return next(err)
 	}
 
+	// decrypt user & pass using base64 data format
 	var auth = new Buffer.from(authheader.split(' ')[1],
 	'base64').toString().split(':');
 	var user = auth[0];
 	var pass = auth[1];
 
-	if (user == '' && pass == '') {
+	// check if the entered user & pass match, if yes, then call next() and display index.html in browser
+	if (user == 'piotr' && pass == 'Test@123!') {
 
 		// If Authorized user
 		next();
@@ -35,9 +39,10 @@ function authentication(req, res, next) {
 
 // First step is the authentication of the client
 app.use(authentication)
+// if authenticaiton is success, then go to public dir and load the html
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Server setup
+// Server setup, and listen on port 3000, and write message to log "Server is Running"
 app.listen((3000), () => {
 	console.log("Server is Running");
 })
